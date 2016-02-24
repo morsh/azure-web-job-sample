6e5c49e0f70049f8e9fa99adb83fa2b814b42f93
morsh <mor.shemesh@gmail.com>
2016-02-24 17:14:49 +0200

    dynamic worker

--- app_data/jobs/continuous/query/worker.js
+++ app_data/jobs/continuous/query/worker.js
@@ -1,3 +1,10 @@
-setInterval(function () {
-  console.log("2 seconds have passed...");
-}, 2000);
+var workerName = 'query-id';//process.env.WORKER;
+
+console.log('Selected worker according to environment variable:', workerName);
+
+var worker = require('x-' + workerName);
+
+worker.run(function (err) {
+    if (err) return console.error('error running', workerName, 'worker:', err);
+    console.info(workerName, 'worker exited');
+});
